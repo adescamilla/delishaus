00112eaf738ce260f982ae2fa29bdff93da78301
Andrew Escamilla <aescami6@stu.southtexascollege.edu>
2023-12-14 23:10:01 -0600

    quick change

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/delishaus/',
+  base: '/final2023/',
   plugins: [vue()],
   resolve: {
     alias: {
